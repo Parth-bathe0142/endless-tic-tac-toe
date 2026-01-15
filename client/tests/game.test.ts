d658d75b2f790cbc1a84dc8571e0240b92197a0d
Parth-bathe0142 <parth.bathe0142@gmail.com>
2026-01-15 18:32:43 +0530

    fixed bugs and added documentation

--- client/tests/game.test.ts
+++ client/tests/game.test.ts
@@ -1,4 +1,4 @@
-import { GameState } from "shared"
+import { Cell, GameState } from "shared"
 
 
 test("creating an empty state", () => {
@@ -13,7 +13,11 @@ test("creating an empty state", () => {
 
 
 test("creating a state from an array, without an expiring cell", () => {
-    const arr = [0, 1, 0, 0, 0, 0, 0, 0, 0]
+    const arr = [
+        0, 1, 0,
+        0, 0, 0,
+        0, 0, 0
+    ]
     
     const state = new GameState(arr)
 
@@ -25,7 +29,11 @@ test("creating a state from an array, without an expiring cell", () => {
 })
 
 test("creating a state from an array, with an expiring cell", () => {
-    const arr = [6, 1, 2, 5, 4, 0, 3, 7, 8]
+    const arr = [
+        6, 1, 2,
+        5, 4, 0,
+        3, 7, 8
+    ]
     
     const state = new GameState(arr)
 
@@ -34,4 +42,58 @@ test("creating a state from an array, with an expiring cell", () => {
     expect(array).toEqual(arr)
     expect(expiring).toBe(1)
     expect(last).toBe(8)
+})
+
+test("test making a move, when nothing is expiring", () => {
+    const arr = [
+        0, 1, 0,
+        0, 0, 2,
+        3, 0, 0
+    ]
+    const expected = [
+        0, 1, 0,
+        0, 0, 2,
+        3, 4, 0
+    ]
+    
+    let state = new GameState(arr)
+    state = state.turn(7)!
+
+    const { array, expiring, last } = state
+    
+    expect(array).toEqual(expected)
+    expect(expiring).toBe(0)
+    expect(last).toBe(4)
+})
+
+test("test making a move, when a move is expiring", () => {
+    const arr = [
+        0, 0, 0,       
+        4, 5, 2,
+        3, 6, 7
+    ]
+    const expected = [
+        0, 0, 8,
+        4, 5, 0,
+        3, 6, 7
+    ]
+    
+    const { X, O, N, E } = Cell
+    const expectedBoard = [
+        [N, N, X,],
+        [X, O, N,],
+        [E, X, O],
+    ]
+    
+    let gameState = new GameState(arr)
+    gameState = gameState.turn(2)!
+
+    const { array, state, expiring, last } = gameState
+    
+    console.log(state)
+    expect(last).toBe(8)
+    expect(expiring).toBe(3)
+
+    expect(state).toEqual(expectedBoard)
+    expect(array).toEqual(expected)
 })
--- shared/index.ts
+++ shared/index.ts
@@ -1,16 +1,77 @@
+/**
+ * X - X
+ * O - O
+ * N - no move here
+ * E - oldest move on the board, will become N
+ *     the next move is made. Whether it was an
+ *     X or an O does not matter as it cannot contribute
+ *     to a win
+ */
 export enum Cell {
     X, O, E, N
 }
 
+/**
+ * Returns the cell variant corresponding to the passed
+ * number. In this game, even numbers are X and odd numbers
+ * are O while 0 is N
+ * 
+ * E variant denotes the current oldest move on the board
+ * which can be either even or odd and needs to be found
+ * seperately
+ * 
+ * @param num The move number.
+ * @returns The corresponding cell variant
+ */
+function cellFromNumber(num: number): Cell {
+    switch (true) {
+        case num == 0:
+            return Cell.N
+            
+        case num % 2 == 0:
+            return Cell.X
+                
+        case num % 2 == 1:
+            return Cell.O
+
+        default:
+            return Cell.N                    
+    }
+}
+
 export class GameState {
-    array: number[] = []
-    state: Cell[][] = [[]]
+    /** The latest move */
     last: number = 0
+    /** 
+     * The oldest move, expiry starts after 6 moves
+     */
     expiring: number = 0
 
+    /**
+     * The array representation of a board. records the moves
+     * in sequence, making it possible to find the oldest move
+     * */
+    array: number[] = []
+    /**
+     * The array parsed into a board for display
+     */
+    state: Cell[][] = [[]]
+
+    /**
+     * creates an empty game with all Ns
+     */
     constructor();
+
+    /**
+     * parses the passed array into a board to play on.
+     * Both the array and the board are stored in the object
+     * to help calculations.
+     * The array representation exists because it is easier to
+     * serialise.
+     * @param array 
+     */
     constructor(array: number[]);
-    
+
     constructor(array?: number[]) {
         if(array) {
             this.fromArray(array)
@@ -19,36 +80,26 @@ export class GameState {
         }
     }
 
-    fromArray(array: number[]) {
+
+    private fromArray(array: number[]) {
         this.array = array
 
         let min = Number.MAX_SAFE_INTEGER;
-        let minPos: [number, number] = [0, 0];
         let max = 0;
+
+        let minPos: [number, number] = [0, 0];
         this.state = new Array(3)
 
+        // create a state from the array while also finding
+        // smallest and largest moves on board
         for (let i = 0; i < 3; i++) {
             this.state[i] = new Array(3)
+
             for (let j = 0; j < 3; j++) {
                 const index = i * 3 + j;
                 let num = array[index];
                 
-                switch (true) {
-                    case num == 0:
-                        this.state[i][j] = Cell.N
-                        break
-                        
-                    case num % 2 == 0:
-                        this.state[i][j] = Cell.X
-                        break
-                            
-                    case num % 2 == 1:
-                        this.state[i][j] = Cell.O
-                        break
-
-                    default:
-                        this.state[i][j] = Cell.N                    
-                }
+                this.state[i][j] = cellFromNumber(num)
 
                 if(num != 0 && num < min) {
                     min = num
@@ -62,6 +113,8 @@ export class GameState {
             }
         }
 
+        // if more than 6 moves have been made so far,
+        // mark the expiring move
         if(max >= 6) {
             this.state[minPos[0]][minPos[1]] = Cell.E
             this.expiring = min
@@ -86,63 +139,45 @@ export class GameState {
         this.state[i][j] = cell
     }
 
+    /**
+     * Makes a move on the desired array index if it is within
+     * bounds. if a move already exists here, returns null.
+     * Whether the move is an X or an O depends on the last move,
+     * ensuring that the same same move cannot be made twice.
+     * 
+     * @param idx The index where a move is to be made
+     * @returns updated state if it was valid, else null
+     */
     turn(idx: number) {
-        if(this.get(idx) == Cell.N) {
+        // return if invalid
+        if(idx < 0 || idx >= 9 || this.get(idx) != Cell.N) {
             return null
         }
 
+        // make the move
         this.last++
-
-        let cell
-        if(this.last % 2 == 0) {
-            cell = Cell.X
-        } else {
-            cell = Cell.O
-        }
+        let cell = cellFromNumber(this.last)
 
         this.array[idx] = this.last
         this.set(idx, cell)
 
+        // If expiry has started, advance it
         if(this.expiring != 0) {
-            let expired
+            let expired = this.array.indexOf(this.expiring)
 
-            for (let i = 0; i < this.array.length; i++) {
-                const c = this.array[i];
-                
-                if(c == this.expiring) {
-                    expired = c
-                    break
-                }
-            }
-
+            // oldest move decays to N
             this.set(expired!, Cell.N)
             this.array[expired!] = 0
 
+            // the now oldest move turns to E
             this.expiring++
-            let newExpiring
-
-            for (let i = 0; i < this.array.length; i++) {
-                const c = this.array[i];
-                
-                if(c == this.expiring) {
-                    newExpiring = c
-                    break
-                }
-            }
-            
+            let newExpiring = this.array.indexOf(this.expiring)
             this.set(newExpiring!, Cell.E)
+
+        // if this is the 6th move, expiry starts
         } else if(this.last == 6) {
             this.expiring++
-            let idx
-
-            for (let i = 0; i < this.array.length; i++) {
-                const c = this.array[i];
-                
-                if(c == this.expiring) {
-                    idx = c
-                    break
-                }
-            }
+            let idx = this.array.indexOf(this.expiring)
 
             this.set(idx!, Cell.E)
         }
